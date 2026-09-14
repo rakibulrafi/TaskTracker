10d80a638d0d4d89e926ff90b6f54c0e8657cc3c
Md Rakibul Alam <rakibulrafi66@gmail.com>
2026-09-14 13:58:12 +0300

    Initial C# TaskTracker project

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("========================");
+Console.WriteLine("       TASK TRACKER");
+Console.WriteLine("========================");
+Console.WriteLine();
+
+Console.WriteLine("Welcome to my C# Task Tracker!");
+Console.WriteLine();
+Console.WriteLine("This project will also teach me Git and GitHub.");
